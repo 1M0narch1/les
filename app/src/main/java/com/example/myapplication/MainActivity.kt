dcee1b100841ee352e18c5f4dbb4d1eed0d1992b
1M0narch1 <131494764+1M0narch1@users.noreply.github.com>
2023-05-09 13:01:24 +0300

    step4

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.Uri
+import android.os.BatteryManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.myapplication.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        fun GetBatteryLevel(context : Context):String{
+            val ifilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+            val batteryStatus = registerReceiver(null, ifilter)
+
+            val level = batteryStatus!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+            val scale = batteryStatus!!.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+
+            val batteryPct = level / scale.toFloat()
+
+            return (batteryPct * 100).toString()
+        }
+
+        binding.BatteryChargeButton.setOnClickListener{
+            binding.BatteryChargeTV.text = GetBatteryLevel(this)
+        }
+
+        binding.SuppotPixelPhoneButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://support.google.com/pixelphone/answer/7015477?hl=en"))
+            startActivity(intent)
+        }
     }
 }
